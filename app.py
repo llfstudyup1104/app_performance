f499891dfb9a1e6449a0cc07975e080cea8a68e9
lli <33363882+llfstudyup1104@users.noreply.github.com>
2019-07-22 14:47:16 +0800

    Update app package

--- app.py
+++ app.py
@@ -1,45 +1,29 @@
 # _*_ coding: utf-8 _*_
 import os
-import time
 
 
 class App(object):
-
-    def __init__(self, page_name, first_activity):
-        """"构造方法"""
-        self.page_name = page_name
-        self.first_activity = first_activity
+    
+    def __init__(self, pagename, activity):
+        self.pagename = pagename
+        self.activity = activity
         self.content = ""
         self.start_time = ""
 
     def start_app(self):
-        """开启APP"""
-        cmd = "adb shell am start -W -n " + self.page_name + self.first_activity
+        cmd = "adb shell am start -W -n " + self.pagename + self.activity
         self.content = os.popen(cmd)
-
-    def cold_stop_app(self):
-        """冷停止APP"""
-        cmd = "adb shell am force-stop " + self.page_name
-        os.popen(cmd)
-
+    
     @staticmethod
-    def warm_stop_app():
-        """热停止app"""
+    def wart_stop():
         cmd = "adb shell input keyevent 3"
         os.popen(cmd)
-
-    def get_launch_time(self):
-        """获取启动时间"""
-        for line in self.content.readlines():
-            if "ThisTime" in line:
-                self.start_time = line.split(":")[1].strip()
-                break
-        return self.start_time
-
-
+        
+    def cold_stop(self):
+        cmd = "adb shell am force-stop " + self.pagename
+        os.popen(cmd)
+        
+    
 if __name__ == '__main__':
     app = App("com.byton.ice.android.multimedia", "/com.byton.ice.android.multimedia.ui.DRTControlActivity")
     app.start_app()
-    time.sleep(2)
-    app.cold_stop_app()
-    print("It costs " + app.get_launch_time() + "ms to start this activity")
