5284c0c6e330ac6e64844b1cbc4b85c56d9f7feb
lli <33363882+llfstudyup1104@users.noreply.github.com>
2019-07-22 14:54:32 +0800

    Add config folder and config.py

---
+++ utils/config.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+__version__ = '0.0.1'
+
+import configparser as ConfigParser
+from manual.utils.logger import logger    
+import os
+import traceback
+
+
+class Configure(object):
+    def __init__(self):
+        self.config = ConfigParser.RawConfigParser()
+        self.configPath = os.path.join(os.path.dirname(__file__), '../config/settings.ini')
+        self.config.read(self.configPath)
+    
+    def get_config_value(self, section, option):
+        try:
+            return self.config.get(section, option)
+        except:
+            logger.error(traceback.format_exc())
+            return None
+        
+    def set_config_value(self, section, option, value):
+        try:
+            for item in self.config.sections():
+                for subItem in self.config.options(item):
+                        self.config.set(item, subItem, self.get_config_value(item, subItem))
+            
+            if not self.config.has_section(section):
+                self.config.add_section(section)
+    
+            self.config.set(section, option, value)
+            
+            with open(self.configPath, 'w') as configfile:    
+                self.config.write(configfile)
+        except:
+            logger.error(traceback.format_exc())
+            logger.error("saving configuration failed")
+
+
+if __name__ == '__main__':
+    cf = Configure()
+    print(cf.get_config_value("app", "page"))
+    print(cf.get_config_value("app", "activity"))
