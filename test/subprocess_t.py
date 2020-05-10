80a71b454407a0b7ac4d0986eb5b5ef050b98feb
longfei.li <longfei.li@byton.com>
2020-05-10 09:17:38 +0800

    add new practice for subprocess

--- test/subprocess_t.py
+++ test/subprocess_t.py
@@ -2,23 +2,17 @@ import subprocess
 import sys, os
 from loguru import logger
 
-def ping_check(host):
-    ret = None
-    cmd = f'ping -c 1 -t 1 -s 1 {host}'
-    # response = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.DEVNULL,
-                                    # shell=True).communicate(timeout=30)[0].decode(sys.getdefaultencoding(), errors='replace').encode().decode('utf-8', errors='replace')
-    response = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.DEVNULL, shell=True).communicate()[0].decode(sys.getdefaultencoding(), errors='replace').encode().decode('utf-8', errors='replace')
+# host = '10.13.2.111'
+# output = subprocess.call(f'ping -c2 {host}', shell=True)
+# print(f'The output is {output}')
 
-    if 'ttl=' in response.lower():
-        logger.debug("ping succeeded for {0}".format(host))
-        ret = True
+# try:
+#     subprocess.check_call('lsd -t', shell=True)
+# except subprocess.CalledProcessError as err:
+#     print("Command err")
 
-def ping_check2(host):
-    ret = None
-    cmd = f'ping -c 1 -t 1 -s 1 {host}'
-    res = os.popen(cmd)
-    if res:
-        for line in res.readlines():
-            if 'ttl' in line:
-                logger.info("Ping check success")
-ping_check2('10.13.2.111')
+
+try:
+    output = subprocess.check_output('it - l', shell=True, stderr=subprocess.STDOUT)
+except subprocess.CalledProcessError as err:
+    print(f"Command error: {err}")
--- test/test.py
+++ test/test.py
@@ -1,7 +1,9 @@
-from stat import S_ISDIR as isdir
+from stat import S_ISDIR, S_ISREG
 import paramiko
+from loguru import logger
 
-local_dir = r'/tmp/llf/1.log'
+local_file = r'/tmp/llf/1.log'
+local_dir = r'/tmp/llf'
 host = '10.13.2.111'
 user = 'tester'
 pwd = 'byton[]\\'
@@ -10,7 +12,25 @@ ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
 ssh.connect(hostname=host, username=user, password=pwd)
 sftp = ssh.open_sftp()
-status = sftp.stat(local_dir)
-print(status.st_mode)
+print(sftp.stat(local_dir).st_mode)
+
+if S_ISDIR(sftp.stat(local_dir).st_mode):
+    logger.info("目录")
+else:
+    logger.warning("Unknown")
+
 ssh.close()
 sftp.close()
+
+# print(os.stat(local_file).st_mode)
+# print(os.stat(local_dir).st_mode)
+
+# if S_ISDIR(os.stat(local_dir).st_mode):
+#     logger.info("目录")
+# else:
+#     logger.info("Unknown")
+
+# if S_ISREG(os.stat(local_file).st_mode):
+#     logger.info("文件")
+# else:
+#     logger.info("Unknown")
